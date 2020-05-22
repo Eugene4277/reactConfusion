eafae5d50644eb4aeffb86df45ad5222cac27ac4
Eugene Stepanenko <stepanenko.yevheniy@gmail.com>
2020-05-21 19:31:58 -0700

    Drawer Navigator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { View, Platform } from "react-native";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+import Home from "./HomeComponent";
 
 const MenuNavigator = createStackNavigator(
   {
@@ -24,6 +24,45 @@ const MenuNavigator = createStackNavigator(
   }
 );
 
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#d1c4e9",
+  }
+);
+
 class Main extends Component {
   render() {
     return (
@@ -34,7 +73,7 @@ class Main extends Component {
             Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
         }}
       >
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
