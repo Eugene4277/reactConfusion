0c05c8b7d532c03d425852e5306ac8ce10542ebb
Eugene Stepanenko <stepanenko.yevheniy@gmail.com>
2020-05-21 20:04:18 -0700

    Navigation part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ScrollView, Text } from "react-native";
+import { Card } from "react-native-elements";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+import { DISHES } from "../shared/dishes";
+
+function RenderItem(props) {
+  const item = props.item;
+
+  if (item != null) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        featuredSubtitle={item.designation}
+        image={require("./images/uthappizza.png")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  } else {
+    return <View></View>;
+  }
+}
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
+    };
+  }
+
   static navigationOptions = {
     title: "Home",
   };
 
   render() {
     return (
-      <View>
-        <Text>Home Component</Text>
-      </View>
+      <ScrollView>
+        <RenderItem
+          item={this.state.dishes.filter((dish) => dish.featured)[0]}
+        />
+        <RenderItem
+          item={this.state.promotions.filter((promo) => promo.featured)[0]}
+        />
+        <RenderItem
+          item={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      </ScrollView>
     );
   }
 }
