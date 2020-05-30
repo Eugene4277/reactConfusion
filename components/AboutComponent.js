c84ba29a1d08685a38fb080c0040cf2d20d982c5
Eugene Stepanenko <stepanenko.yevheniy@gmail.com>
2020-05-30 14:47:15 -0700

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -68,16 +69,20 @@ class About extends Component {
       );
     } else if (this.props.leaders.errMess) {
       <ScrollView>
-        <RenderHistory />
-        <Card title="Corporate Leadership">
-          <Text>{this.props.leaders.errMess}</Text>
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <RenderHistory />
+          <Card title="Corporate Leadership">
+            <Text>{this.props.leaders.errMess}</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>;
     } else {
       return (
         <ScrollView>
-          <RenderHistory />
-          <RenderLeaders leaders={this.props.leaders.leaders} />
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <RenderHistory />
+            <RenderLeaders leaders={this.props.leaders.leaders} />
+          </Animatable.View>
         </ScrollView>
       );
     }
